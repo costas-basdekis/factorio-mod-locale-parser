6a7ac3fea6c6d01ac1ba2fd0a8db6f282b6fb10c
Costas Basdekis <costas.basdekis@gmail.com>
2022-05-05 14:11:47 +0100

    Load mod locale data from API

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python3
 import json
 import re
+import tempfile
 from pathlib import Path
 import zipfile
 import configparser
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, Tuple, Iterator, Iterable
+from tqdm import tqdm
+import requests
+
+API_AUTH_FILE = Path("./api_auth.json")
 
 FACTORIO_ROOT = Path.home() / ".factorio"
 FACTORIO_MOD_ROOT = FACTORIO_ROOT / "mods"
 
+MOD_SETTINGS_DATA_PATH = Path("./mod_settings_data.json")
+MOD_SETTINGS_DATA_TEMPORARY_PATH = Path("./mod_settings_data.temporary.json")
 
 RE_INFO = re.compile(r"[^/]+/info.json", re.IGNORECASE)
 RE_LOCALE_PATH = re.compile(r"[^/]+/locale/([\w-]+)/[^/]+.cfg")
 
 
 def main():
-    mod_data, locale_data = get_mods_settings_locale_data(FACTORIO_MOD_ROOT)
+    update_mod_locale_data()
+
+
+def update_mod_locale_data():
+    mod_data, locale_data = get_mods_settings_locale_data(
+        FACTORIO_MOD_ROOT, False)
+
+    print(len(mod_data), "mods")
+    print(len(locale_data), "settings")
+    save_mod_settings_data(mod_data, locale_data)
+
+
+def save_mod_settings_data(
+        mod_data: Dict, locale_data: Dict, temporary: bool = False) -> None:
     locales = sorted({
         locale
         for setting_data in locale_data.values()
         for by_mod_and_language in setting_data["by_mod_and_language"].values()
         for locale in by_mod_and_language
     })
-
-    print(len(mod_data), "mods")
-    print(len(locale_data), "settings")
-    with open("./mod_settings_data.json", "w") as f:
+    if temporary:
+        path = MOD_SETTINGS_DATA_TEMPORARY_PATH
+    else:
+        path = MOD_SETTINGS_DATA_PATH
+    with open(path, "w") as f:
         json.dump({
             "locales": locales,
             "mods": mod_data,
@@ -33,22 +54,34 @@ def main():
         }, f, indent=2)
 
 
-def get_mods_settings_locale_data(factorio_mod_root: Path) -> Tuple[Dict, Dict]:
-    mod_zips = [
-        item
-        for item in factorio_mod_root.glob("*_*.zip")
-        if item.is_file()
-    ]
-    print(f"Found {len(mod_zips)} mod zips")
-    mod_data = {}
-    locale_data = {}
-    for mod_zip in mod_zips:
-        zip_file = zipfile.ZipFile(mod_zip)
+def get_mods_settings_locale_data(
+    factorio_mod_root: Path, local: bool,
+) -> Tuple[Dict, Dict]:
+    if MOD_SETTINGS_DATA_TEMPORARY_PATH.exists():
+        with open(MOD_SETTINGS_DATA_TEMPORARY_PATH, "r") as f:
+            all_data = json.load(f)
+            mod_data = all_data["mods"]
+            locale_data = all_data["settings"]
+    else:
+        mod_data = {}
+        locale_data = {}
+
+    if local:
+        zip_files = iterate_local_mod_zip_files(factorio_mod_root)
+    else:
+        zip_files = iterate_zip_files_from_api(excluding_mods=mod_data)
+
+    for index, zip_file in enumerate(zip_files):
+        if index % 10 == 0:
+            print(f"Opened {index} zip files")
+        # It was skipped
+        if not zip_file:
+            continue
         mod_info = get_mod_info(zip_file)
         if not mod_info:
             print(
                 f"Could not find info file, name, or title, in mod ZIP "
-                f"{mod_zip}")
+                f"{zip_file.filename}")
             continue
         mod_name, mod_title = mod_info
         mod_data[mod_name] = {
@@ -57,8 +90,99 @@ def get_mods_settings_locale_data(factorio_mod_root: Path) -> Tuple[Dict, Dict]:
             "setting_names": [],
         }
         get_mod_settings_locale_data(zip_file, mod_name, locale_data, mod_data)
+        save_mod_settings_data(mod_data, locale_data, temporary=True)
+
+    if MOD_SETTINGS_DATA_TEMPORARY_PATH.exists():
+        MOD_SETTINGS_DATA_TEMPORARY_PATH.unlink()
 
     return mod_data, locale_data
+
+
+def iterate_local_mod_zip_files(
+        factorio_mod_root: Path) -> Iterator[zipfile.ZipFile]:
+    mod_zips = [
+        item
+        for item in factorio_mod_root.glob("*_*.zip")
+        if item.is_file()
+    ]
+    print(f"Found {len(mod_zips)} mod zips locally")
+    for mod_zip in mod_zips:
+        zip_file = zipfile.ZipFile(mod_zip)
+        yield zip_file
+
+
+MOD_URL_ROOT = "https://mods.factorio.com"
+
+
+def iterate_zip_files_from_api(
+        excluding_mods: Iterable[str] = ()) -> Iterator[zipfile.ZipFile]:
+    try:
+        with API_AUTH_FILE.open("rb") as f:
+            api_auth = json.load(f)
+    except FileNotFoundError:
+        raise Exception(
+            f"Missing API auth file at {API_AUTH_FILE} - copy "
+            f"`player-data.json` to that path, copy the service username and "
+            f"token to that file from `player-data.json`, or set the path at "
+            f"`API_AUTH_FILE`")
+    for mod_api_data in iterate_mods_from_api(excluding_mods=excluding_mods):
+        if not mod_api_data:
+            # It was skipped
+            yield None
+            continue
+        download_url = (mod_api_data.get('latest_release', {}) or {})\
+            .get('download_url')
+        if not download_url:
+            continue
+        zip_url = (
+            f"{MOD_URL_ROOT}"
+            f"{download_url}"
+            f"?username={api_auth['service-username']}"
+            f"&token={api_auth['service-token']}"
+        )
+        response = requests.get(zip_url, stream=True)
+        with tempfile.NamedTemporaryFile() as tmp:
+            progress_bar = tqdm(
+                desc=(
+                    f"Downloading {mod_api_data['title']} "
+                    f"({mod_api_data['name']})"
+                ),
+                total=int(response.headers.get('content-length', 0)),
+                unit='iB', unit_scale=True)
+            try:
+                for data in response.iter_content(10 * 1024 * 1024):
+                    progress_bar.update(len(data))
+                    tmp.write(data)
+            finally:
+                progress_bar.close()
+            tmp.flush()
+            tmp.seek(0)
+            try:
+                zip_file = zipfile.ZipFile(tmp)
+            except zipfile.BadZipfile:
+                raise Exception(
+                    "File was not a ZIP - did you provide a valid username and "
+                    "token?")
+            yield zip_file
+
+
+MOD_API_URL_ROOT = "https://mods.factorio.com/api/mods"
+
+
+def iterate_mods_from_api(excluding_mods: Iterable[str] = ()):
+    next_url = MOD_API_URL_ROOT
+    while next_url:
+        response = requests.get(next_url)
+        data = response.json()
+        for item in data["results"]:
+            if item["name"] in excluding_mods:
+                print(f"Skipping {item['name']}")
+                # Let upstream know that we're skipping one, so that they can
+                # keep track of the count so far
+                yield None
+                continue
+            yield item
+        next_url = data["pagination"]["links"]["next"]
 
 
 def get_mod_info(zip_file: zipfile.ZipFile) -> Optional[Tuple[str, str]]:
@@ -71,8 +195,11 @@ def get_mod_info(zip_file: zipfile.ZipFile) -> Optional[Tuple[str, str]]:
     if not info_filenames:
         return None
     info_filename = info_filenames[0]
-    with zip_file.open(info_filename) as f:
-        info_data = json.load(f)
+    try:
+        with zip_file.open(info_filename) as f:
+            info_data = json.load(f)
+    except UnicodeDecodeError:
+        return None
     if not isinstance(info_data, dict) \
             or "name" not in info_data or "title" not in info_data:
         return None
@@ -100,7 +227,7 @@ def get_mod_settings_locale_data(
             except UnicodeDecodeError:
                 print(
                     f"Could not parse {locale_name} locale file "
-                    f"{locale_filename} or {mod_name}")
+                    f"{locale_filename} of {mod_name}")
                 continue
             if "\ufeff" in config_text:
                 config_text = config_source.decode("utf-8-sig")
@@ -122,6 +249,9 @@ def get_settings_locale_from_config(
         config.read_string(config_text)
     except configparser.MissingSectionHeaderError:
         config.read_string("[DEFAULT]\r\n" + config_text)
+    except configparser.ParsingError:
+        print(f"Could not read config file for {mod_name}")
+        return
 
     def add_setting_label(_setting_name, _setting_label):
         setting_data = locale_data.setdefault(_setting_name, {
